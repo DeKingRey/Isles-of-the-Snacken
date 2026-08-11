64627857dc60012806e42c6daff2600d5c97ec92
DeKingRey <tigermiguel456@gmail.com>
2026-08-11 14:39:41 +1200

    UI and Coins

--- Assets/Scripts/Managers/QuotaManager.cs
+++ Assets/Scripts/Managers/QuotaManager.cs
@@ -1,9 +1,15 @@
+using System.Collections;
 using TMPro;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class QuotaManager : NetworkBehaviour
 {
+    public static QuotaManager Instance;
+    [Tooltip("The wait time between feeding the Snacken and finding out the result")]
+    [SerializeField] private float checkDuration = 10f;
+
     [Header("Quota")]
     [SerializeField] private TextMeshProUGUI quotaText;
 
@@ -16,6 +22,19 @@ public class QuotaManager : NetworkBehaviour
     private NetworkVariable<int> deliveredNommians = new(0);
     private NetworkVariable<int> requiredNommians = new();
 
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
     public override void OnNetworkSpawn()
     {
         // Assign on quota changed so that text isn't constantly updating
@@ -27,6 +46,24 @@ public class QuotaManager : NetworkBehaviour
         // Required nommians (for the quota) increments each level
         requiredNommians.Value = Random.Range(minNommians, maxNommians + 1) 
                                 + (GameManager.Instance.currentLevel * quotaMultiplier);
+    }
+
+    // This is called when the players feed the Snacken
+    public IEnumerator CheckQuotaReached(int amount)
+    {
+        yield return new WaitForSeconds(checkDuration);
+
+        if (amount < requiredNommians.Value)
+        {
+            // Lose :(
+            GameManager.Instance.ChangeState(GameManager.GameState.GameOver);
+        } else
+        {
+            // Win!
+            // Spawn coins
+            SceneEventBus.Instance.ToggleLoadingScreenRpc(true);
+            NetworkManager.Singleton.SceneManager.LoadScene("Game", LoadSceneMode.Single);
+        }
     }
 
     public override void OnNetworkDespawn()
